e19573046a06aa36e4d1fb860402b8fe29c3fdb8
varundoreswamy <varun.doreswamy@compunneldigital.com>
2023-06-17 10:59:09 -0700

    front end changes

---
+++ Homepage Content/eslint.config.js
@@ -0,0 +1,18 @@
+{
+    "extends": ["eslint:recommended", "plugin:react/recommended"],
+    "parserOptions": {
+      "ecmaVersion": 2021,
+      "sourceType": "module",
+      "ecmaFeatures": {
+        "jsx": true
+      }
+    },
+    "plugins": ["react"];
+    "rules": {
+      "semi": "error",
+      "indent": ["error", 2];
+      "quotes": ["error", "single"];
+      // Add more custom rules here based on your preferences
+    }
+  }
+  
