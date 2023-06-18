594a9ee70f4eec4ca1c89205ff1f24d59771380e
Weedsociety <103718091+Weedsociety@users.noreply.github.com>
2023-06-18 03:02:36 -0700

    Front End 5

--- frontend/src/components/CardDeal.jsx
+++ frontend/src/components/CardDeal.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
 
-function CardDeal() {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" />in a few steps.</h2>
+      <p className={``}>yada yada yada</p>
+    </div>
+  </section>
+)
 
 export default CardDeal
