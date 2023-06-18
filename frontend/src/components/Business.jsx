1c3174154608dbf3f8979b3918547a1610790f6f
Weedsociety <103718091+Weedsociety@users.noreply.github.com>
2023-06-18 02:28:38 -0700

    frontend update 4

--- frontend/src/components/Business.jsx
+++ frontend/src/components/Business.jsx
@@ -2,12 +2,36 @@ import { features } from '../constants';
 import styles, { layout } from '../style';
 import Button from './Button';
 
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length -1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+    <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+      {title}
+    </h4>
+    <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+      {content}
+    </p>
+    </div>
+  </div>
+)
+
 const Business = () => {
   return (
     <section id="features" className={layout.section}>
       <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>In The Battle of Ai Vs. DeepFakes, <br className="sm:block hidden" /> We Are The Winning Team</h2>
-        <p></p>
+        <h2 className={styles.heading2}>In The Battle of <br /> Ai Vs. DeepFakes, <br className="sm:block hidden" /> We Are The <br /> Winning Team</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>As deepfakes advance, they meet their match in our cutting-edge Generative Adversarial Networks (GANs). We specialize in utilizing GANs to unmask and combat deepfakes, ensuring the integrity of digital content.</p>
+
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id}{...feature} index={index}/>
+        ))}
       </div>
     </section>
   )
